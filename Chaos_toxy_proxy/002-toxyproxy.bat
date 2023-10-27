119b46e5bf5c521c1fde5ba8743b84a8d9d2449e
phaniannadanam <113410300+phaniannadanam@users.noreply.github.com>
2023-10-28 00:23:54 +0530

    Update 002-toxyproxy.bat

--- Chaos_toxy_proxy/002-toxyproxy.bat
+++ Chaos_toxy_proxy/002-toxyproxy.bat
@@ -7,7 +7,7 @@ REM Give a moment for toxiproxy to start up
 timeout /t 5
 
 REM Add a proxy for our nginx server
-curl -X POST localhost:8474/proxies -d "{\"name\": \"nginx_proxy\",\"listen\": \"[::]:8080\",\"upstream\": \"nginx-victim:80\"}"
+curl -X POST localhost:8474/proxies -d "{ \"name\": \"nginx_proxy\", \"listen\": \"[::]:8080\", \"upstream\": \"nginx-victim:80\" }"
 
 REM Introduce chaos (e.g., latency of 1000ms)
-curl -X POST localhost:8474/proxies/nginx_proxy/toxics -d "{\"name\": \"latency_downstream\",\"type\": \"latency\",\"stream\": \"downstream\",\"latency\": 1000}"
+curl -X POST localhost:8474/proxies/nginx_proxy/toxics -d "{ \"name\": \"latency_downstream\", \"type\": \"latency\", \"stream\": \"downstream\", \"latency\": 1000 }"
