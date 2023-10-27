8aabd7f04f4424311728cc6444ce7b93c7621cf1
phaniannadanam <113410300+phaniannadanam@users.noreply.github.com>
2023-10-28 01:00:57 +0530

    Update 002-toxyproxy.bat

--- Chaos_toxy_proxy/002-toxyproxy.bat
+++ Chaos_toxy_proxy/002-toxyproxy.bat
@@ -1,13 +1,22 @@
 @echo off
 
-REM Create the Toxiproxy container
+REM Create the Toxiproxy container and bind it to the host's port 8080
 docker run -d --name toxiproxy -p 8474:8474 -p 8080:8080 shopify/toxiproxy:latest
 
 REM Give a moment for toxiproxy to start up
 timeout /t 5
 
 REM Add a proxy for our nginx server
-curl -X POST localhost:8474/proxies -d "{ \"name\": \"nginx_proxy\", \"listen\": \"[::]:8080\", \"upstream\": \"nginx-victim:80\" }"
+docker exec toxiproxy curl -X POST localhost:8474/proxies -d '{
+  "name": "nginx_proxy",
+  "listen": "[::]:8080",
+  "upstream": "nginx-victim:80"
+}'
 
 REM Introduce chaos (e.g., latency of 1000ms)
-curl -X POST localhost:8474/proxies/nginx_proxy/toxics -d "{ \"name\": \"latency_downstream\", \"type\": \"latency\", \"stream\": \"downstream\", \"latency\": 1000 }"
+docker exec toxiproxy curl -X POST localhost:8474/proxies/nginx_proxy/toxics -d '{
+  "name": "latency_downstream",
+  "type": "latency",
+  "stream": "downstream",
+  "latency": 1000
+}'
