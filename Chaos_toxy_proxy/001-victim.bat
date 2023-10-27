aaeb670ccbb3a0067a01ae3dfe5c54a2dd3f9a05
phaniannadanam <113410300+phaniannadanam@users.noreply.github.com>
2023-10-28 01:00:13 +0530

    Update 001-victim.bat

--- Chaos_toxy_proxy/001-victim.bat
+++ Chaos_toxy_proxy/001-victim.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Create the victim based on the "nginx:latest" image and expose its port to the host on port 8080
+REM Create the victim based on the "nginx:latest" image WITHOUT exposing its port to the host
 echo Creating Victim: nginx-victim
-docker run -itd -p 8080:80 --name nginx-victim nginx:latest
+docker run -itd --name nginx-victim nginx:latest
 echo Creating nginx-victim DONE!!!
