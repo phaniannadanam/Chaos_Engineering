3ebaa67620b3891c314d54c7cdb66ad20e07461d
phaniannadanam <113410300+phaniannadanam@users.noreply.github.com>
2023-10-28 06:30:39 +0530

    Update 001-victim.bat

--- Chaos_toxy_proxy/001-victim.bat
+++ Chaos_toxy_proxy/001-victim.bat
@@ -1,6 +1,20 @@
 @echo off
+setlocal
 
-REM Create the victim based on the "nginx:latest" image WITHOUT exposing its port to the host
-echo Creating Victim: nginx-victim
-docker run -itd --name nginx-victim nginx:latest
-echo Creating nginx-victim DONE!!!
+:: Create a volume for nginx content
+docker volume create nginx-content
+
+:: Setting paths based on the current directory of the batch script
+set "SCRIPT_DIR=%~dp0"
+set "INDEX_PATH=%SCRIPT_DIR%index.html"
+set "NGINX_CONFIG_PATH=%SCRIPT_DIR%nginx.conf"
+
+:: Copy custom index.html and nginx.conf to the Docker volume
+docker run --rm -v nginx-content:/data -v %INDEX_PATH%:/index.html busybox cp /index.html /data/index.html
+docker run --rm -v nginx-content:/data -v %NGINX_CONFIG_PATH%:/nginx.conf busybox cp /nginx.conf /data/nginx.conf
+
+:: Run the nginx victim container
+docker run -itd --name nginx-victim -v nginx-content:/usr/share/nginx/html:ro -v nginx-content:/etc/nginx:ro nginx:latest
+
+echo "Nginx Victim container started."
+endlocal
