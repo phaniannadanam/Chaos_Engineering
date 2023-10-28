a3b90a59e9df138d62d90c2ca553c95094f3074b
phaniannadanam <113410300+phaniannadanam@users.noreply.github.com>
2023-10-28 07:19:20 +0530

    Add files via upload

---
+++ Chaos_toxy_proxy/001-buildimage.bat
@@ -0,0 +1,2 @@
+:: Build the custom NGINX Docker image
+docker build -t custom-nginx-image .
---
+++ Chaos_toxy_proxy/002-runcustomcontainer.bat
@@ -0,0 +1,2 @@
+:: Run the custom NGINX Docker container
+docker run -d --name custom-nginx-container -p 80:80 custom-nginx-image
